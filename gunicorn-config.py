7bf9dc3bf72341f3c560911700e1a190ad58a815
Dan Shernicoff <dan@brassnet.biz>
2024-10-04 09:31:18 -0400

    Setup config file for gunicorn.

---
+++ gunicorn-config.py
@@ -0,0 +1,16 @@
+import logging
+import os
+
+logger = logging.getLogger('config')
+
+if os.path.exists('certs/certs.py'):
+    # Since we don't want to have secrets in Git we'll import them from a separate directory.
+    import certs.certs as certs
+else:
+    logger.warning('No certs config found')
+    certs = None
+
+bind = '0.0.0.0:8000'  # The IP:port to bind to. Using '0.0.0.0' says to accept from any IP.
+workers = 2  # The number of worker processes to run
+certfile = getattr(certs, 'certfile', None)  #
+keyfile = getattr(certs, 'keyfile', None)
